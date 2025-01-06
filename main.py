b35824dbd32f4b0d101bf750409473da3ec0525c
tidjaha <hamza.tidjani@yahoo.fr>
2025-01-06 13:07:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ def predict_tc(input_features_tc):
     # Make predictions using the loaded model
 
     poly_tc = poly_tc.transform(input_features_tc)
-    input_features_tc[""Tc(K)""] = model_LR_n2_tc.predict(poly_tc)
+    input_features_tc["Tc(K)"] = model_LR_n2_tc.predict(poly_tc)
     prediction=scaler_tc_tc.inverse_transform(input_features_tc[""Tc(K)""].reshape(-1, 1))
 
     # Return the predictions
